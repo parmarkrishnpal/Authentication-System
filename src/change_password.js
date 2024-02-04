53635bd6b7e02ebdedf350e7224d4241b9c3b737
cw-krishnpal-parmar <krishnpal@carwale.com>
2024-02-04 17:00:52 +0530

    Refactored change password code

--- src/change_password.js
+++ src/change_password.js
@@ -6,45 +6,46 @@ const JWT_SECRET = process.env.JWT_SECRET;
 async function change_password(req, res) {
   const { token, newpassword: plainTextPassword } = req.body;
 
-  if (!plainTextPassword || typeof plainTextPassword !== "string") {
-    return res.json({
+  // Validate password
+  if (
+    !plainTextPassword ||
+    typeof plainTextPassword !== "string" ||
+    plainTextPassword.length < 6
+  ) {
+    return res.status(400).json({
       status: "error",
-      error: "Invalid password",
-    });
-  }
-
-  if (plainTextPassword.length < 5) {
-    return res.json({
-      status: "error",
-      error: "Password too small. Should be atleast 6 characters",
+      error: "Invalid password. Should be at least 6 characters",
     });
   }
 
   try {
+    // Verify JWT token
     const user = jwt.verify(token, JWT_SECRET);
-
     const _id = user.id;
 
-    const password = await bcrypt.hash(plainTextPassword, 10);
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(plainTextPassword, 10);
 
+    // Update user password
     await User.updateOne(
       {
         _id,
       },
       {
         $set: {
-          password,
+          password: hashedPassword,
         },
       }
     );
+
     res.json({
       status: "ok",
     });
   } catch (error) {
-    console.log(error);
-    res.json({
+    console.error("Error changing password:", error);
+    res.status(500).json({
       status: "error",
-      error: ";))",
+      error: "Internal Server Error",
     });
   }
 }
